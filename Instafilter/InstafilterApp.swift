3720f375b9020dc027cbd5f770c8c949175b44ce
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-20 15:18:40 +0800

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Giovanni Gaffé on 2021/11/20.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
