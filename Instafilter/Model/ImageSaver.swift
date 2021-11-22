f461cabba1582e3d2875d587177f2e6ecb6e9b16
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-22 14:31:19 +0800

    set new file image saver

---
+++ Instafilter/Model/ImageSaver.swift
@@ -0,0 +1,18 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Giovanni Gaffé on 2021/11/22.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished")
+    }
+}
