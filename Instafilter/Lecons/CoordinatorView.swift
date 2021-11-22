4b2791326f53b5d20dc567ee5077484c950b8a05
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-22 09:16:01 +0800

    set coordiantor view

---
+++ Instafilter/Lecons/CoordinatorView.swift
@@ -0,0 +1,20 @@
+//
+//  CoordinatorView.swift
+//  Instafilter
+//
+//  Created by Giovanni Gaffé on 2021/11/21.
+//
+
+import SwiftUI
+
+struct CoordinatorView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct CoordinatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoordinatorView()
+    }
+}
